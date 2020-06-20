bc33a5b083714b8f0935c197fe69c2730b3a5d5a
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-06-21 03:51:48 +0800

    Create sendMail utility function

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import { CONTACT_API_URL } from '../constants/form'
+
+export const sendEmail = async (
+    name: string,
+    email: string,
+    message: string
+): Promise<ContactAPIResponse> => {
+    console.log('\n--- Sending Email ---')
+
+    const response = await axios.post(CONTACT_API_URL, { name, email, message })
+
+    return response.data as ContactAPIResponse
+}
