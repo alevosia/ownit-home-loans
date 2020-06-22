6d1a59f1715411365868f5d2a6d0ee6202f287c2
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-06-22 23:55:33 +0800

    Removed styled-theming

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,23 +1,14 @@
-import React /*, { useState }*/ from 'react'
-// import { ThemeProvider } from 'styled-components'
+import React from 'react'
 import { AppWrapper } from './App.styles'
 
 // pages
 import FormPage from '../../pages/Form/Form.page'
 
 const App: React.FC = () => {
-    // const [theme, setTheme] = useState<AppTheme>({ mode: 'light' })
-
-    // function changeTheme() {
-    //     setTheme({ mode: theme.mode === 'light' ? 'dark' : 'light' })
-    // }
-
     return (
-        // <ThemeProvider theme={theme}>
         <AppWrapper id="App">
             <FormPage />
         </AppWrapper>
-        // </ThemeProvider>
     )
 }
 
--- src/components/App/App.styles.ts
+++ src/components/App/App.styles.ts
@@ -1,22 +1,11 @@
 import styled from 'styled-components'
-import theme from 'styled-theming'
 
 import { Colors } from '../../constants'
 
-const backgroundColor = theme('mode', {
-    light: Colors.WHITE,
-    dark: Colors.BLACK
-})
-
-const color = theme('mode', {
-    light: Colors.BLACK,
-    dark: Colors.WHITE
-})
-
 export const AppWrapper = styled.div`
     position: relative;
-    color: ${color};
-    background-color: ${backgroundColor};
+    color: ${Colors.DARK_BLUE};
+    background-color: ${Colors.WHITE};
     height: 100%;
     width: 100%;
     transition: background-color 1s ease-in-out;
