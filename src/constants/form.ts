546042f7dc9ec70490b499880c3d9b6675566af5
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-06-23 01:33:11 +0800

    Update MESSAGE_SUCCESS

--- src/constants/form.ts
+++ src/constants/form.ts
@@ -1,8 +1,8 @@
 export enum Messages {
-    MESSAGE_SUCCESS = `Great news! Ownit Home Loans is committed to striving for a Conditional Home ` +
-        `Loan Approval based on the stats you have provided! We will be in touch to ` +
-        `get started and one step closer to your dream home today!`,
-    MESSAGE_FAILED = 'Failed to send your message. Please try again.',
+    MESSAGE_SUCCESS = `Welcome! You’re now one step closer to your financial goals.`,
+    MESSAGE_SUCCESS2 = `Ownit Home Loans is committed to striving for a Conditional Home Loan Approval based ` +
+        `on the stats you have provided! We will be in touch to get started and one step closer to your dream home today!`,
+    MESSAGE_FAILED = 'Failed to send your message. Refresh the page to try again.',
     MESSAGE_ERROR = 'Something went wrong. Please try again later.',
     MESSAGE_SENDING = 'Sending...'
 }
