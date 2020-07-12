2748fe0dcc466f67e6f462817ae1afe37b371fa9
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-07-13 04:36:47 +0800

    Scroll to top on page change

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -3,11 +3,13 @@ import { Wrapper } from './App.styles'
 
 import { Routes } from '../../routes'
 import { DrawerProvider } from '../../contexts/drawer'
+import { ScrollToTop } from '../../routes'
 
 const App: React.FC = () => {
     return (
         <Wrapper id="App">
             <DrawerProvider>
+                <ScrollToTop />
                 <Routes />
             </DrawerProvider>
         </Wrapper>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Switch, Route, useLocation } from 'react-router-dom'
 
 // pages
 import HomePage from '../pages/Home'
 import FormPage from '../pages/Form'
 import CalculatorsPage from '../pages/Calculators'
 import ContactUsPage from '../pages/ContactUs'
+
+// automatically scrolls to top on page change
+export const ScrollToTop: React.FC = () => {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+
+    return null
+}
 
 export const Routes: React.FC = () => {
     return (
