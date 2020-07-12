2c0b2037177bc9d3fb9f16afeea39f501ea5de34
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-07-12 19:18:53 +0800

    Change all calculator src to https

--- src/constants/calculators.ts
+++ src/constants/calculators.ts
@@ -1,163 +1,163 @@
 export const LoanRepaymentCalculator: Calculator = {
     id: 'Loan_Repayment_Calculator',
     name: 'Loan Repayment Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/G100/Loan_Repayment_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/G100/Loan_Repayment_Calculator/FBAA'
 }
 
 export const BorrowingPowerCalculator: Calculator = {
     id: 'Borrowing_Power_Calculator',
     name: 'Borrowing Power Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A200/Borrowing_Power_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A200/Borrowing_Power_Calculator/FBAA'
 }
 
 export const BudgetPlanner: Calculator = {
     id: 'Budget_Planner',
     name: 'Budget Planner',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A400/Budget_Planner/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A400/Budget_Planner/FBAA'
 }
 
 export const ComparisonRateCalculator: Calculator = {
     id: 'Comparison_Rate_Calculator',
     name: 'Comparison Rate Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A300/Comparison_Rate_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A300/Comparison_Rate_Calculator/FBAA'
 }
 
 export const CompoundInterestCalculator: Calculator = {
     id: 'Compound_Interest_Calculator',
     name: 'Compound Interest Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A500/Compound_Interest_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A500/Compound_Interest_Calculator/FBAA'
 }
 
 export const CreditCardCalculator: Calculator = {
     id: 'Credit_Card_Calculator',
     name: 'Credit Card Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A500/Credit_Card_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A500/Credit_Card_Calculator/FBAA'
 }
 
 export const ExtraRepaymentCalculator: Calculator = {
     id: 'Extra_Repayment_Calculator',
     name: 'Extra Repayment Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A300/Extra_Repayment_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A300/Extra_Repayment_Calculator/FBAA'
 }
 
 export const HomeLoanOffsetCalculator: Calculator = {
     id: 'Home_Loan_Offset_Calculator',
     name: 'Home Loan Offset Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A300/Home_Loan_Offset_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A300/Home_Loan_Offset_Calculator/FBAA'
 }
 
 export const SavingCalculator: Calculator = {
     id: 'Saving_Calculator',
     name: 'Saving Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/G200/Saving_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/G200/Saving_Calculator/FBAA'
 }
 
 export const InterestOnlyMortgageCalculator: Calculator = {
     id: 'Interest_Only_Mortgage_Calculator',
     name: 'Interest Only Mortgage Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/G100/Interest_Only_Mortgage_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/G100/Interest_Only_Mortgage_Calculator/FBAA'
 }
 
 export const IntroductoryRateLoanCalculator: Calculator = {
     id: 'Introductory_Rate_Loan_Calculator',
     name: 'Introductory Rate Loan Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A300/Introductory_Rate_Loan_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A300/Introductory_Rate_Loan_Calculator/FBAA'
 }
 
 export const LumpSumRepaymentCalculator: Calculator = {
     id: 'Lump_Sum_Repayment_Calculator',
     name: 'Lump Sum Repayment Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A300/Lump_Sum_Repayment_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A300/Lump_Sum_Repayment_Calculator/FBAA'
 }
 
 export const SplitLoanCalculator: Calculator = {
     id: 'Split_Loan_Calculator',
     name: 'Split Loan Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A300/Split_Loan_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A300/Split_Loan_Calculator/FBAA'
 }
 
 export const LeasingCalculator: Calculator = {
     id: 'Leasing_Calculator',
     name: 'Leasing Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A300/Leasing_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A300/Leasing_Calculator/FBAA'
 }
 
 export const LoanComparisonCalculator: Calculator = {
     id: 'Loan_Comparison_Calculator',
     name: 'Loan Comparison Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A300/Loan_Comparison_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A300/Loan_Comparison_Calculator/FBAA'
 }
 
 export const PropertyBuyingCostCalculator: Calculator = {
     id: 'Property_Buying_Cost_Calculator',
     name: 'Property Buying Cost Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A400/Property_Buying_Cost_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A400/Property_Buying_Cost_Calculator/FBAA'
 }
 
 export const PropertySellingCostCalculator: Calculator = {
     id: 'Property_Selling_Cost_Calculator',
     name: 'Property Selling Cost Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A400/Property_Selling_Cost_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A400/Property_Selling_Cost_Calculator/FBAA'
 }
 
 export const ReverseMortgageCalculator: Calculator = {
     id: 'Reverse_Mortgage_Calculator',
     name: 'Reverse Mortgage Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A500/Reverse_Mortgage_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A500/Reverse_Mortgage_Calculator/FBAA'
 }
 
 const IncomeTaxCalculator: Calculator = {
     id: 'Income_Tax_Calculator',
     name: 'Income Tax Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A100/Income_Tax_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A100/Income_Tax_Calculator/FBAA'
 }
 
 export const StampDutyCalculator: Calculator = {
     id: 'Stamp_Duty_Calculator',
     name: 'Stamp Duty Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A200/Stamp_Duty_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A200/Stamp_Duty_Calculator/FBAA'
 }
 
 export const IncomeAnnualisationCalculator: Calculator = {
     id: 'Income_Annualisation_Calculator',
     name: 'Income Annualisation Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A100/Income_Annualisation_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A100/Income_Annualisation_Calculator/FBAA'
 }
 
 export const IncomeGrossUpCalculator: Calculator = {
     id: 'Income_Gross_Up_Calculator',
     name: 'Income Gross Up Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/A100/Income_Gross_Up_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/A100/Income_Gross_Up_Calculator/FBAA'
 }
 
 export const HowLongtoSaveCalculator: Calculator = {
     id: 'How_Long_to_Save_Calculator',
     name: 'How Long to Save Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/G200/How_Long_to_Save_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/G200/How_Long_to_Save_Calculator/FBAA'
 }
 
 export const HowMuchtoDepositCalculator: Calculator = {
     id: 'How_Much_to_Deposit_Calculator',
     name: 'How Much to Deposit Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/G200/How_Much_to_Deposit_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/G200/How_Much_to_Deposit_Calculator/FBAA'
 }
 
 export const FortnightlyRepaymentCalculator: Calculator = {
     id: 'Fortnightly_Repayment_Calculator',
     name: 'Fortnightly Repayment Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/G100/Fortnightly_Repayment_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/G100/Fortnightly_Repayment_Calculator/FBAA'
 }
 
 export const MortgageSwitchingCalculator: Calculator = {
     id: 'Mortgage_Switching_Calculator',
     name: 'Mortgage Switching Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/G300/Mortgage_Switching_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/G300/Mortgage_Switching_Calculator/FBAA'
 }
 
 export const RentvsBuyCalculator: Calculator = {
     id: 'Rent_vs_Buy_Calculator',
     name: 'Rent vs Buy Calculator',
-    src: 'http://www.visionabacus.net/Tools/B3/SuiteA/G300/Rent_vs_Buy_Calculator/FBAA'
+    src: 'https://www.visionabacus.net/Tools/B3/SuiteA/G300/Rent_vs_Buy_Calculator/FBAA'
 }
 
 interface CalculatorConstants {
